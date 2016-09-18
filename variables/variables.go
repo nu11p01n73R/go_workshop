32fbe82cc9d415d20be64582dfdb803628ae39f2
nu11p01n73R <kjjohny1991@gmail.com>
2016-09-18 11:16:56 +0530

    variable usages

---
+++ variables/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var aInt int
+	aInt = 20
+	fmt.Println("aInt = ", aInt)
+
+	aString := "Go Programing Language"
+	fmt.Println("aString = ", aString)
+}
