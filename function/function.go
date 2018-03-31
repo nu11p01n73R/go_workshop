0fa6bcf089179eae5d5bbd22419a54fa4daf6074
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-03-31 11:34:50 +0200

    Fix typo

--- function/function.go
+++ function/function.go
@@ -17,7 +17,7 @@ func divide(a, b int) (int, int) {
 
 func division(a, b int) (int, error) {
 	if b == 0 {
-		return 0, errors.New("Division by zero is not possilbe")
+		return 0, errors.New("Division by zero is not possible")
 	}
 	return a / b, nil
 }
